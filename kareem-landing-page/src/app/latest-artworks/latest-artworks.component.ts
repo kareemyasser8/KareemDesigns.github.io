492268363dfa81fb75fbc2e3cf779e6db3c0b7b8
kareemyasser8 <kareem.yasserr@gmail.com>
2023-01-29 20:18:37 +0200

    adding IntersectionObserver

--- kareem-landing-page/src/app/latest-artworks/latest-artworks.component.ts
+++ kareem-landing-page/src/app/latest-artworks/latest-artworks.component.ts
@@ -1,17 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { fadeIn, startSlide } from '../animations';
 
+
+
 @Component({
   selector: 'latest-artworks',
   templateUrl: './latest-artworks.component.html',
   styleUrls: ['./latest-artworks.component.scss'],
-  animations:[startSlide,fadeIn]
+  animations: [startSlide, fadeIn]
 })
+
+
 export class LatestArtworksComponent implements OnInit {
 
   constructor() { }
 
+  makeFadeOnScroll(){
+    let images = document.querySelectorAll('.fade-in');
+    let options = {
+      rootMargin: "0px 0px -100px 0px"
+    };
+    let observer = new IntersectionObserver(
+      (entries, observer)=>{
+        entries.forEach(entry=>{
+          if(!entry.isIntersecting) return;
+          else{
+            entry.target.classList.add("appear");
+            observer.unobserve(entry.target);
+          }
+        })
+      },options)
+
+    images.forEach(image => {
+      observer.observe(image)
+  });
+
+  }
+
   ngOnInit(): void {
+    this.makeFadeOnScroll()
   }
 
 }
