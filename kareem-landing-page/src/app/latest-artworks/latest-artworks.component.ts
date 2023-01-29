a4dae316cf51a0dd6021307db6e16c298377a09f
kareemyasser8 <kareem.yasserr@gmail.com>
2023-01-29 14:01:27 +0200

    adding parralax in cover

--- kareem-landing-page/src/app/latest-artworks/latest-artworks.component.ts
+++ kareem-landing-page/src/app/latest-artworks/latest-artworks.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeIn, startSlide } from '../animations';
 
 @Component({
   selector: 'latest-artworks',
   templateUrl: './latest-artworks.component.html',
-  styleUrls: ['./latest-artworks.component.scss']
+  styleUrls: ['./latest-artworks.component.scss'],
+  animations:[startSlide,fadeIn]
 })
 export class LatestArtworksComponent implements OnInit {
 
