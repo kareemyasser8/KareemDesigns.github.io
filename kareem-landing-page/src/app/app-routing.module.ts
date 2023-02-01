79d37ef16f3f81eb7acca6d824e8c98e4f69a9c8
kareemyasser8 <kareem.yasserr@gmail.com>
2023-02-01 22:06:52 +0200

    attempt1

--- kareem-landing-page/src/app/app-routing.module.ts
+++ kareem-landing-page/src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
-import { GalleryPageComponent } from './gallery-page/gallery-page.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactPageComponent } from './contact-page/contact-page.component';
 
-const routes: Routes = [
-  {path: '', redirectTo: 'home-page', pathMatch: 'full'},
-  {path: 'home-page', component: HomePageComponent},
-  {path: 'gallery-page', component: GalleryPageComponent},
-  {path: 'contact-page', component: ContactPageComponent},
-
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
